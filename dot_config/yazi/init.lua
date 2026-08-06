a9a1ebcefe404f59644ca781d460077c1b9a3adc
Vladimir Mikryukov <v.mikryukov@gmail.com>
2026-08-06 21:02:57 +0300

    yazi lua - add mtime to status bar

--- dot_config/yazi/init.lua
+++ dot_config/yazi/init.lua
@@ -1,11 +1,30 @@
-
--- Show symlimk in status bar
+-- Show symlink in status bar
 Status:children_add(function(self)
-  local h = self._current.hovered
-  if h and h.link_to then
-    return " -> " .. tostring(h.link_to)
-  else
-    return ""
-  end
+	local h = self._current.hovered
+	if h and h.link_to then
+		return " -> " .. tostring(h.link_to)
+	else
+		return ""
+	end
 end, 3300, Status.LEFT)
 
+-- Show modification time near permissions (right side)
+Status:children_add(function(self)
+	local h = self._current.hovered
+	if not h then
+		return ""
+	end
+	local time = math.floor(h.cha.mtime or 0)
+	if time == 0 then
+		return ""
+	end
+	return ui.Line { " ", ui.Span(os.date("%Y-%m-%d %H:%M", time)), " " }
+end, 1500, Status.RIGHT)
+
+require("symlink-target-path"):setup {
+	normalize = true,
+	skip_broken = false,
+}
+
+-- Required for duckdb.yazi (parquet previewer)
+-- require("duckdb"):setup()
